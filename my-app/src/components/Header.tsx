d209b5e52cba110afd17d2a97e70c273290d34e9
Zach Barber <zachbarber321@gmail.com>
2020-02-18 21:57:03 -0500

    created router and components

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Nav, Navbar, } from 'react-bootstrap'
+import Home from './Home'
+import AboutUs from './AboutUs';
+import Contact from './Contact';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink
+} from "react-router-dom";
+
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <Router>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand>Steele Barber Engineering</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Link className="nav-link" to='/'>Home</Link>
+                            <Link className="nav-link" to='/about'>About Us</Link>
+                            <Link className="nav-link" to='/contact'>Contact</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+
+                <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={AboutUs} />
+                <Route path="/contact" component={Contact} />
+                </Switch>
+
+            </Router>
+        )
+    }
+
+}
+
+export default Header;
